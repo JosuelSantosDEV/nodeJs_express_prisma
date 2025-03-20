0e7b9241a70c65bad01c88eac31723b4178b4174
JosuelSantosDEV <josuelsantosdev@gmail.com>
2025-03-19 23:09:25 -0300

    Sending project to GitHub

---
+++ src/router.ts
@@ -0,0 +1,15 @@
+import { Router } from "express"
+
+
+const router = Router()
+
+router.get("/status", async (req, res, next) => {
+  try {
+    
+    res.json({ message: "OK" })
+  } catch (error) {
+    next(error)
+  }
+})
+
+export { router }
